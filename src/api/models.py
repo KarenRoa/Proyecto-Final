168e59a9062330f4c4aeac734b196c184d772a90
Jhonny Mendoza <jhomen11@gmail.com>
2021-12-29 00:00:06 +0000

    Se crea modelo de cuidadores

--- src/api/models.py
+++ src/api/models.py
@@ -2,18 +2,60 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Cuidador(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(120), unique=False, nullable=False)
+    apellido = db.Column(db.String(120), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+    telefono = db.Column(db.String(80), unique=False, nullable=False)
+    comuna = db.Column(db.String(80), unique=False, nullable=False)
+    rrss = db.Column(db.String(80), unique=False, nullable=True)
+    descripcion = db.Column(db.String, unique=False, nullable=False)
+    
+    # def __repr__(self):
+    #     return '<User %r>' % self.username
 
     def serialize(self):
         return {
             "id": self.id,
+            "nombre": self.nombre,
+            "apellido": self.apellido,
             "email": self.email,
+            "password": self.password,
+            "telefono": self.telefono,
+            "comuna": self.comuna,
+            "rrss": self.rrss,
+            "descripcion": self.descripcion,
+
+            # do not serialize the password, its a security breach
+        }
+
+class Clien(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(120), unique=False, nullable=False)
+    apellido = db.Column(db.String(120), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    telefono = db.Column(db.String(80), unique=False, nullable=False)
+    comuna = db.Column(db.String(80), unique=False, nullable=False)
+    rrss = db.Column(db.String(80), unique=False, nullable=True)
+    descripcion = db.Column(db.String, unique=False, nullable=False)
+    
+    # def __repr__(self):
+    #     return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "apellido": self.apellido,
+            "email": self.email,
+            "password": self.password,
+            "telefono": self.telefono,
+            "comuna": self.comuna,
+            "rrss": self.rrss,
+            "descripcion": self.descripcion,
+
             # do not serialize the password, its a security breach
         }
