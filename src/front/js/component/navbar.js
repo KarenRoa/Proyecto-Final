12c0a15ab99c9ba6e69fd7dc3f72c0ec21704896
KarenRoa <roaaguayokaren@gmail.com>
2021-12-30 22:54:31 +0000

    con Navbar de Fernando

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,58 +1,65 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "../../styles/navbar.css";
-import { faDog } from "@fortawesome/free-solid-svg-icons";
+import perro from "../../img/perro2.png";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-secondary text-light">
-      <FontAwesomeIcon icon={faDog} />
-      <div className="container-fluid">
-        <a className="navbar-brand text-white" href="#">
-          MATCHPET
-        </a>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark" id="navb">
+      <a className="navbar-brand" id="logo">
+        <Link to="/" className="subrayado">
+          <img
+            src={perro}
+            width="35"
+            height="35"
+            className="d-inline-block align-top"
+            alt=""
+          />
+          <strong href="#" id="titulo">
+            WALK-DOGS
+          </strong>
+        </Link>
+      </a>
 
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a
-                className="nav-link active text-white"
-                aria-current="page"
-                href="#"
-              >
-                INICIO
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-white" href="#">
-                QUIENES SOMOS
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-white" href="#">
-                CONTACTO
-              </a>
-            </li>
-            <p>
-              <button type="button" className="btn btn-outline-light">
-                INGRESAR
-              </button>
-            </p>
-          </ul>
-        </div>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              INICIO
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              QUIENES SOMOS
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              CONTACTO
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div id="f-r">
+        <Link to="/SignUp">
+          <button type="button" className="btn btn-outline-light">
+            INGRESAR
+          </button>
+        </Link>
       </div>
     </nav>
+
+    /*<nav className="navbar navbar-light bg-light">
+			<div className="container">
+				<Link to="/">
+					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				</Link>
+				<div className="ml-auto">
+					<Link to="/demo">
+						<button className="btn btn-primary">Check the Context in action</button>
+					</Link>
+				</div>
+			</div>
+		</nav>*/
   );
 };
