9d43efdec3077bb34bae70a754f3d501fd5fbe59
KarenRoa <roaaguayokaren@gmail.com>
2021-12-30 22:29:15 +0000

    Home

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,54 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../../styles/navbar.css";
+import {faDog } from "@fortawesome/free-solid-svg-icons";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-secondary text-light">
+		<FontAwesomeIcon icon={faDog} />
+      <div className="container-fluid">
+       
+        <a className="navbar-brand text-white" href="#">
+          MATCHPET
+        </a>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" ></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active text-white" aria-current="page" href="#">
+                INICIO
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#">
+                QUIENES SOMOS
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#">
+                CONTACTO
+              </a>
+              
+            </li>
+            <p >
+            <button type="button" className="btn btn-outline-light">INGRESAR</button>
+            </p>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
