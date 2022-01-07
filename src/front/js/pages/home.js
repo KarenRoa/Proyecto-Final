cc808cf0b423b7a9608869bec1d043f89ee18415
Carolina Araya <ci.arayagonzalez@gmail.com>
2022-01-07 00:17:09 +0000

    cambios en home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,6 +1,10 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
+import foto from "../../img/perfil01.jpg";
+import foto1 from "../../img/perfil02.jpg";
+import foto2 from "../../img/perfil03.png";
+import foto3 from "../../img/perfil04.png";
 import { Link, useParams } from "react-router-dom";
 
 export const Home = () => {
@@ -24,14 +28,15 @@ export const Home = () => {
         <div className="container-fluid">
           <div className="row mb-5">
             <div className="col-sm-6 col-md-6 col-lg-3">
-              <div className="h-100 p-5 bg-light rounded-3 shadow-sm">
+              <div className="h-100 p-2 bg-light rounded-3 shadow-sm">
                 <img
-                  src="https://images.blush.design/QuHK5J2yl2vqKxCdOHSc?w=920&auto=compress&cs=srgb"
+                  src={foto}
                   width="200"
                   height="200"
                   className="rounded-circle"
                 ></img>
-                <h3 className="fw-light">nombre</h3>
+                <hr className="my-2" />
+                <h4 className="fw-light">nombre</h4>
                 <hr className="my-2" />
                 <p className="fw-light">
                   <small>
@@ -45,14 +50,15 @@ export const Home = () => {
               </div>
             </div>
             <div className="col-sm-6 col-md-6 col-lg-3">
-              <div className="h-100 p-5 bg-light rounded-3 shadow-sm">
-              <img
-                  src="https://images.blush.design/QuHK5J2yl2vqKxCdOHSc?w=920&auto=compress&cs=srgb"
+              <div className="h-100 p-2 bg-light rounded-3 shadow-sm">
+                <img
+                  src={foto1}
                   width="200"
                   height="200"
                   className="rounded-circle"
                 ></img>
-                <h3 className="fw-light">nombre</h3>
+                <hr className="my-2" />
+                <h4 className="fw-light">nombre</h4>
                 <hr className="my-2" />
                 <p className="fw-light">
                   <small>
@@ -66,14 +72,15 @@ export const Home = () => {
               </div>
             </div>
             <div className="col-sm-6 col-md-6 col-lg-3">
-              <div className="h-100 p-5 bg-light rounded-3 shadow-sm">
-              <img
-                  src="https://images.blush.design/QuHK5J2yl2vqKxCdOHSc?w=920&auto=compress&cs=srgb"
+              <div className="h-100 p-2 bg-light rounded-3 shadow-sm">
+                <img
+                  src={foto2}
                   width="200"
                   height="200"
                   className="rounded-circle"
                 ></img>
-                <h3 className="fw-light">nombre</h3>
+                <hr className="my-2" />
+                <h4 className="fw-light">nombre</h4>
                 <hr className="my-2" />
                 <p className="fw-light">
                   <small>
@@ -87,14 +94,15 @@ export const Home = () => {
               </div>
             </div>
             <div className="col-sm-6 col-md-6 col-lg-3">
-              <div className="h-100 p-5 bg-light rounded-3 shadow-sm">
-              <img
-                  src="https://images.blush.design/QuHK5J2yl2vqKxCdOHSc?w=920&auto=compress&cs=srgb"
+              <div className="h-100 p-2 bg-light rounded-3 shadow-sm">
+                <img
+                  src={foto3}
                   width="200"
                   height="200"
                   className="rounded-circle"
                 ></img>
-                <h3 className="fw-light">nombre</h3>
+                <hr className="my-2" />
+                <h4 className="fw-light">nombre</h4>
                 <hr className="my-2" />
                 <p className="fw-light">
                   <small>
@@ -122,14 +130,32 @@ export const Home = () => {
           <h1 className="text-uppercase fst-italic">quienes somos</h1>
         </div>
       </div>
-      <div className="col-sm-6 col-md-6">
-        <img
-          src="https://blush.design/api/download?shareUri=6ERJt5MPgrc3rKqt&w=800&h=800&fm=png"
-          className=""
-          width="600"
-          height="600"
-        />
+      <div className="container-fluid">
+        <div className="row">
+          <div className="boxed1 col-md-6">
+            <img
+              className="mujer"
+              src="https://blush.design/api/download?shareUri=6ERJt5MPgrc3rKqt&w=800&h=800&fm=png"
+            />
+            <img
+              className="arbusto"
+              src="https://blush.design/api/download?shareUri=-V5lYT3tcwHM1ms4&w=800&h=800&fm=png"
+            />
+          </div>
+          <div className="boxed2 col-md-6">
+            <h1 className="display-1">"</h1>
+            <span className="">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare
+              vitae sapien faucibus et vitae velit aenean purus. Fermentum cras
+              facilisi habitant elementum diam dignissim pellentesque nibh
+              molestie. Phasellus mattis pharetra enim non.
+            </span>
+            <h1 className="display-1">"</h1>
+          </div>
+        </div>
       </div>
+
+      <hr className="my-4" />
     </div>
   );
 };
