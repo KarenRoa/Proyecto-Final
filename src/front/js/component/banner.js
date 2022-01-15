ef6a3908204763b3a2a69c5e84eddbb2793766cf
Carolina Araya <ci.arayagonzalez@gmail.com>
2022-01-15 17:13:27 +0000

    Actualizacion de galeria

--- src/front/js/component/banner.js
+++ src/front/js/component/banner.js
@@ -1,31 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
-export const Banner = ()=>{
-
-    return (
-        <div className="container-fluid">
-            <div className="text-center">
-          <h1 className="display-6 fw-lighter">
-            ¿Estás Ocupado o con poco tiempo?
-          </h1>
-          <p className="col fs-4 fw-light text-center">
-            Aquí podrás encontrar a tu{" "}
-            <span className="text-warning fw-bolder">paseador@</span> de perro
-            ideal
-          </p>
-          <p className="col fs-4 fw-light text-center">ó</p>
-          <Link to={"/registroCuidador"}>
-            <div className="boton-paseador">
-              <button className="btn btn-outline-dark btn-sm fw-light shadow-sm mb-5">
-                ¿QUIERES SER{" "}
-                <span className="text-warning fw-bolder">PASEADOR@</span> DE
-                PERRO?
-              </button>
-            </div>
-          </Link>
-        </div>
-        </div>
-    )
-}
+export const Banner = () => {
+  return (
+    <div className="container-fluid">
+      <div className="text-center">
+        <h1 className="display-6 fw-lighter">
+          ¿Estás Ocupado o con poco tiempo?
+        </h1>
+        <p className="col fs-4 fw-light text-center">
+          Aquí podrás encontrar a tu{" "}
+          <span className="text-warning fw-bolder">paseador@</span> de perro
+          ideal
+        </p>
+        <p className="col fs-4 fw-light text-center">ó</p>
+        <Link to={"/registroCuidador"}>
+          <div className="boton-paseador">
+            <button className="btn btn-outline-dark btn-sm fw-light shadow-sm mb-5">
+              ¿QUIERES SER{" "}
+              <span className="text-warning fw-bolder">PASEADOR@</span> DE
+              PERRO?
+            </button>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
